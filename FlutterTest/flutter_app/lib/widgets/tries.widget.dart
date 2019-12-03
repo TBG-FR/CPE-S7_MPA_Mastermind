cd157343a2ff02f27b032657d8fc698658ebe28f
Julien Moorat <contact@julienmoorat.fr>
2019-12-03 22:25:43 +0100

    Auto scroll on new try element

--- FlutterTest/flutter_app/lib/widgets/tries.widget.dart
+++ FlutterTest/flutter_app/lib/widgets/tries.widget.dart
@@ -1,4 +1,6 @@
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_app/model/mastermind.model.dart';
 import 'try.widget.dart';
@@ -16,6 +18,34 @@ class TriesWidget extends StatefulWidget {
 class _TriesWidgetState extends State<TriesWidget>
 {
 
+  ScrollController _scrollController;
+  int localTriesCount = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
+      if (mounted ) {
+        if (localTriesCount != widget.mastermind.tries.length) {
+          localTriesCount = widget.mastermind.tries.length;
+          _scrollToBottom();
+        }
+      } else {
+        timer.cancel();
+      }
+    });
+  }
+
+  void _scrollToBottom() {
+
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      curve: Curves.easeOut,
+      duration: const Duration(milliseconds: 300),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -23,6 +53,7 @@ class _TriesWidgetState extends State<TriesWidget>
       child: ListView.builder(
           itemCount: widget.mastermind.tries.length,
           shrinkWrap: true,
+          controller: _scrollController,
           //         padding: const EdgeInsets.all(1.0),
           itemBuilder: (BuildContext context, int index) {
             return new TryWidget(
